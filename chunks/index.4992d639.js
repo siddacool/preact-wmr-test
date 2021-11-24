2d475b44fad85547b61a0f15d55e81fb650b83af
Siddhesh Mangela <sidthecool007@gmail.com>
2021-11-24 15:33:29 +0530

    deploy 🔥

---
+++ chunks/index.4992d639.js
@@ -0,0 +1,1 @@
+import{s,m as e}from"../index.c0ee15d2.js";null;const t="about_b2ki7r";export default({query:s})=>e`<section class=${t}><h1>About</h1><p>A page all about this website.</p><pre>${JSON.stringify(s)}</pre></section>`;
